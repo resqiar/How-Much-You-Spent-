568e3e76d74e41ad5e44c3a8e554a2f8dbe574e6
resqiar <resqiageng@gmail.com>
2020-11-12 19:46:47 +0700

    Add a model for TXHistory

---
+++ lib/models/Transaction.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final String desc;
+  final double amount;
+  final DateTime time;
+
+  Transaction({
+      @required this.id,
+      @required this.title,
+      @required this.desc, 
+      @required this.amount,
+      @required this.time
+    })
+}
